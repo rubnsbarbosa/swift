abe41a0caf7122c6010cccdb0c50c3ac35357210
Rubens Barbosa <rubensbarbosa90@gmail.com>
2017-04-29 23:00:52 -0300

    more let

---
+++ swift-tour/variableInitial.swift
@@ -0,0 +1,23 @@
+let label = "The width is "
+let width = 94 
+let widthLabel = label + String(width)
+print(widthLabel)
+
+let apples = 3
+let oranges = 5
+let appleSummary = "I have \(apples) apples."
+let fruitSummary = "I have \(apples + oranges) pieces of fruit."
+print(appleSummary)
+print(fruitSummary)
+
+
+var shoppingList = ["fish", "water", "blue paint"]
+print(shoppingList)
+shoppingList[1] = "bottle of water"
+print(shoppingList)
+
+var occupations = [
+	"Malcolm": "Captain",
+	"Kaylee": "Mechanic"
+]
+print(occupations)
